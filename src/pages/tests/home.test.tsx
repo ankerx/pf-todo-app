e421f9ef5c0f27e5d9cb859be392ba6cf9415000
ankerx <93661548+ankerx@users.noreply.github.com>
2022-09-08 16:02:17 -0400

    feat: husky, prettier, lint setup

--- src/pages/tests/home.test.tsx
+++ src/pages/tests/home.test.tsx
@@ -1,7 +1,7 @@
+import { screen } from "@testing-library/react";
+
 import App from "../../App";
 import { renderWithProviders } from "../../core/utils/test-utils";
-import { screen } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
 
 test("redirecting if user is not authenticated", async () => {
   renderWithProviders(<App />, { route: "/" });
